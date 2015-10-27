17c7221fd1c914fe212ec4775aeb8dbcfea7217b
Mutanoball <allkaline@yandex.ru>
2015-10-27 16:07:40 +0000

    changed class names

--- Public/Header.php
+++ Public/Header.php
@@ -7,20 +7,20 @@
         
     <?php    
         if ($page == 1) 
-            {echo'<p class="boton2">Список предложений</p>';} 
-            else  {echo '<a href="List.php" class="boton">Список предложений</a>';}
+            {echo'<p class="menu-item_state_active">Список предложений</p>';} 
+            else  {echo '<a href="List.php" class="menu-item">Список предложений</a>';}
         if ($page == 2) 
-            {echo'<p class="boton2">Поиск</p>';} 
-            else  {echo '<a href="Search.php" class="boton">Поиск</a>';}
+            {echo'<p class="menu-item_state_active">Поиск</p>';} 
+            else  {echo '<a href="Search.php" class="menu-item">Поиск</a>';}
         if ($page == 3) 
-            {echo'<p class="boton2">Контакты</p>';} 
-            else  {echo '<a href="Contacts.php" class="boton">Контакты</a>';}
+            {echo'<p class="menu-item_state_active">Контакты</p>';} 
+            else  {echo '<a href="Contacts.php" class="menu-item">Контакты</a>';}
         if ($page == 4) 
-            {echo'<p class="boton2">Войти</p>';} 
-            else  {echo '<a href="Auth.php" class="boton">Войти</a>';}
+            {echo'<p class="menu-item_state_active">Войти</p>';} 
+            else  {echo '<a href="Auth.php" class="menu-item">Войти</a>';}
         if ($page == 5) 
-            {echo'<p class="boton2">На карте</p>';} 
-            else  {echo '<a href="List.php" class="boton">На карте</a>';}
+            {echo'<p class="menu-item_state_active">На карте</p>';} 
+            else  {echo '<a href="Onmap.php" class="menu-item">На карте</a>';}
 
     ?> 
         </div>
