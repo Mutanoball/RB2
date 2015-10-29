5741eae1dceaea5de9504c2c16116cb31b3be901
Mutanoball <allkaline@yandex.ru>
2015-10-29 15:37:35 +0000

    добавил ключи в БД

--- fixtures.sql
+++ fixtures.sql
@@ -23,8 +23,9 @@ DROP TABLE IF EXISTS `District`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `District` (
-  `iddistrict` int(11) DEFAULT NULL,
-  `district` text
+  `iddistrict` int(11) NOT NULL DEFAULT '0',
+  `district` text,
+  PRIMARY KEY (`iddistrict`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -34,7 +35,7 @@ CREATE TABLE `District` (
 
 LOCK TABLES `District` WRITE;
 /*!40000 ALTER TABLE `District` DISABLE KEYS */;
-INSERT INTO `District` VALUES (1,'Stepnoy'),(2,'Tsentr'),(3,'Vostochny'),(4,'Yuzhny');
+INSERT INTO `District` VALUES (1,'Ð¡Ñ‚ÐµÐ¿Ð½Ð¾Ð¹'),(2,'Ð¦ÐµÐ½Ñ‚Ñ€'),(3,'Ð’Ð¾ÑÑ‚Ð¾Ñ‡Ð½Ñ‹Ð¹'),(4,'Ð®Ð¶Ð½Ñ‹Ð¹');
 /*!40000 ALTER TABLE `District` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -46,7 +47,7 @@ DROP TABLE IF EXISTS `Supply`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Supply` (
-  `id` int(11) DEFAULT NULL,
+  `id` int(11) NOT NULL DEFAULT '0',
   `price` int(11) DEFAULT NULL,
   `area` int(11) DEFAULT NULL,
   `metercost` int(11) DEFAULT NULL,
@@ -54,7 +55,8 @@ CREATE TABLE `Supply` (
   `storey` int(11) DEFAULT NULL,
   `stage` text,
   `iddistrict` int(11) DEFAULT NULL,
-  `date` text
+  `date` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -64,7 +66,7 @@ CREATE TABLE `Supply` (
 
 LOCK TABLES `Supply` WRITE;
 /*!40000 ALTER TABLE `Supply` DISABLE KEYS */;
-INSERT INTO `Supply` VALUES (1,2200000,60,36100,2,1,'1',1,'29.10.2015'),(2,2450000,72,33600,2,2,'2',2,'29.10.2015'),(3,3180000,86,36900,3,5,'3',4,'29.10.2015'),(4,1560000,53,29000,2,3,'3',3,'29.10.2015'),(5,1640000,43,38100,1,6,'2',1,'29.10.2015'),(6,1320000,43,30400,1,14,'3',4,'29.10.2015'),(7,2450000,72,34000,3,11,'1',3,'29.10.2015'),(8,2685000,87,30800,3,10,'1',3,'29.10.2015'),(9,2990000,75,39400,2,12,'2',2,'29.10.2015'),(10,3200000,98,32400,3,8,'2',4,'29.10.2015'),(11,2860000,85,33400,3,7,'2',1,'29.10.2015'),(12,3450000,95,36000,3,8,'3',1,'29.10.2015'),(13,2650000,91,29000,3,1,'3',1,'29.10.2015'),(14,2670000,79,33700,3,9,'1',3,'29.10.2015'),(15,2220000,61,36200,2,5,'1',4,'29.10.2015');
+INSERT INTO `Supply` VALUES (1,2200000,60,36100,2,1,'1',1,'2015-10-28'),(2,2450000,72,33600,2,2,'2',2,'2015-10-28'),(3,3180000,86,36900,3,5,'3',4,'2015-10-28'),(4,1560000,53,29000,2,3,'3',3,'2015-10-28'),(5,1640000,43,38100,1,6,'2',1,'2015-10-28'),(6,1320000,43,30400,1,14,'3',4,'2015-10-28'),(7,2450000,72,34000,3,11,'1',3,'2015-10-28'),(8,2685000,87,30800,3,10,'1',3,'2015-10-28'),(9,2990000,75,39400,2,12,'2',2,'2015-10-28'),(10,3200000,98,32400,3,8,'2',4,'2015-10-28'),(11,2860000,85,33400,3,7,'2',1,'2015-10-28'),(12,3450000,95,36000,3,8,'3',1,'2015-10-28'),(13,2650000,91,29000,3,1,'3',1,'2015-10-28'),(14,2670000,79,33700,3,9,'1',3,'2015-10-28'),(15,2220000,61,36200,2,5,'1',4,'2015-10-28');
 /*!40000 ALTER TABLE `Supply` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -77,4 +79,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-10-29  7:38:11
+-- Dump completed on 2015-10-29 15:35:42
