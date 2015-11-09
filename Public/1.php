a2712e0771ea208ce0d78abf9b0662219337b0c7
Mutanoball <allkaline@yandex.ru>
2015-11-09 15:06:34 +0000

    добавил 1.php

--- Public/1.php
+++ Public/1.php
@@ -1,5 +1,11 @@
 <?php
 echo getenv("CLEARDB_DATABASE_URL");
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"]; echo $server;
+$username = $url["user"]; echo $username;
+$password = $url["pass"]; echo $password;
+$db = substr($url["path"], 1); echo $db;
 ?>
 
 <!DOCTYPE html>
