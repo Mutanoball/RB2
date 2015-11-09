2cb86c8b0b220bfedc5fa3346582f4767b5f9aac
Mutanoball <allkaline@yandex.ru>
2015-11-09 14:46:37 +0000

    добавил 1.php

---
+++ Public/1.php
@@ -0,0 +1,14 @@
+<?php
+echo getenv("CLEARDB_DATABASE_URL");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title></title>
+    </head>
+    <body>
+        
+    </body>
+</html>
