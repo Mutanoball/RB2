0f09684a362aeec3e179cd760a9c3995872d4fc6
Mutanoball <allkaline@yandex.ru>
2015-11-10 18:11:15 +0000

    убрал сылку на стр предложения (сс)

--- Public/Offer.php
+++ Public/Offer.php
@@ -43,7 +43,7 @@
     $num=$row['id'];
     echo "<br><br><br><br><br><br>";
     echo "<div class ='list2'><div class='bignum'>$num</div>";
-    echo "<div class ='liist' style='font-size: 20pt' ><a href='Offer.php?id=$num'> $rooms комнатная квартира</a> $area кв.м <br>Ценa: $price рублей<br>Цена за кв. метр: $metercost рублей<br>$storey этаж<br>$district район <br>Дата публикации: $date<br></div>  <img width='250' height='250' src='01.jpg'><div id='map' style='width: 250px; height: 250px'></div></Div>\n";
+    echo "<div class ='liist' style='font-size: 20pt' >$rooms комнатная квартира</a> $area кв.м <br>Ценa: $price рублей<br>Цена за кв. метр: $metercost рублей<br>$storey этаж<br>$district район <br>Дата публикации: $date<br></div>  <img width='250' height='250' src='01.jpg'><div id='map' style='width: 250px; height: 250px'></div></Div>\n";
     
     }
     }
